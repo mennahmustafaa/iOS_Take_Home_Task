0ed9b5fd8a6e133fdd5e607d591dfb1621305e3c
Mennah-allah Mustafa Aboelsoud <115899133+mennahmustafaa@users.noreply.github.com>
2026-09-15 04:59:17 -0700

    Initial Commit

---
+++ iOS task/iOS_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_taskApp.swift
+//  iOS task
+//
+//  Created by Mennah on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct iOS_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
